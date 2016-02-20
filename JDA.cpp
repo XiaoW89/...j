dcf5715de74c9083f25dcceed49d10083dfbb714
binwang <155123671@qq.com>
2016-02-20 19:38:05 +0800

    Establish dev branch

---
+++ JDA.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include "JDA.h"
+
+
+JDA::JDA(){}
+JDA::JDA(const _Parameters& pm)
+{
+	_pm = pm;
+}
+
+JDA::~JDA(){}
---
+++ JDA.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "utils.h"
+
+class JDA
+{
+private:
+	_Parameters _pm;
+
+public:
+	JDA();
+	JDA(const _Parameters&);
+	~JDA();
+
+
+
+};
+
